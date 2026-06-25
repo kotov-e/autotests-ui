4e7e196e30ac4a68c3cd48798e0a376072482e0e
Eugene Kotov <eug01001@gmail.com>
2026-06-25 14:04:22 +0300

    add pytest on test #1

---
+++ tests/test_authorization.py
@@ -0,0 +1,26 @@
+
+from playwright.sync_api import sync_playwright, expect
+
+def test_wrong_email_or_password_authorization():
+    with sync_playwright() as playwright:
+        browser = playwright.firefox.launch(headless=False)
+        page = browser.new_page()
+
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+
+        input_email = page.get_by_test_id('login-form-email-input').locator('input') # найди по test-id элемент и внутри него input
+        input_email.fill('user.name@gmail.com')
+
+        input_password = page.get_by_test_id('login-form-password-input').locator('input')
+        input_password.fill('Password123')
+
+        button_login = page.get_by_test_id('login-page-login-button')
+        button_login.click()
+
+        wrong_email_or_password_alert = page.get_by_test_id ('login-page-wrong-email-or-password-alert')
+        expect(wrong_email_or_password_alert).to_be_visible()
+        expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
+
+        page.wait_for_timeout(2000)
+
+
---
+++ tests/test_courses.py
@@ -0,0 +1,57 @@
+from playwright.sync_api import sync_playwright, expect
+
+def test_empty_courses_list():
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+        button_registration = page.get_by_test_id('registration-page-registration-button')
+        expect(button_registration).to_be_disabled()
+
+        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        email_input.fill('user.name@gmail.com')
+
+        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        username_input.fill('username')
+
+        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        password_input.fill('password')
+
+        expect(button_registration).not_to_be_disabled()
+        button_registration.click()
+
+        dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+        expect(dashboard_title).to_be_visible()
+
+        context.storage_state(path='browser-state.json')
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+
+        context = browser.new_context(storage_state='browser-state.json')
+
+        page = context.new_page()
+
+        page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
+
+        courses_title = page.get_by_test_id('courses-list-toolbar-title-text')  # проверить селектор
+        expect(courses_title).to_be_visible()
+
+        icon_courses = page.get_by_test_id('courses-list-empty-view-icon')  # проверить селектор
+        expect(icon_courses).to_be_visible()
+
+        courses_list_text_empty = page.get_by_test_id('courses-list-empty-view-title-text')  # проверить селектор
+        expect(courses_list_text_empty).to_be_visible()
+        expect(courses_list_text_empty).to_have_text('There is no results')
+
+        courses_list_empty_view_description_text = page.get_by_test_id(
+            'courses-list-empty-view-description-text')  # проверить селектор
+        expect(courses_list_empty_view_description_text).to_be_visible()
+        expect(courses_list_empty_view_description_text).to_have_text(
+            'Results from the load test pipeline will be displayed here')
+
+        page.wait_for_timeout(2000)
