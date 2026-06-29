de74a7c902b318fd545d0640f134118dc682d67e
Eugene Kotov <eug01001@gmail.com>
2026-06-29 14:18:13 +0300

    add page object model #1

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,24 +1,19 @@
 import pytest
-from playwright.sync_api import expect, Page
+
+from pages.login_page import LoginPage
+
+users_invalid_data = [
+    ("user.name@gmail.com", "password"),
+    ("user.name@gmail.com", "  "),
+    ("  ", "password")
+]
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
-def test_wrong_email_or_password_authorization(chromium_page: Page):
-
-    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
-
-    input_email = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-    input_email.fill('user.name@gmail.com')
-
-    input_password = chromium_page.get_by_test_id('login-form-password-input').locator('input')
-    input_password.fill('Password123')
-
-    button_login = chromium_page.get_by_test_id('login-page-login-button')
-    button_login.click()
-
-    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
-    expect(wrong_email_or_password_alert).to_be_visible()
-    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
-
-    chromium_page.wait_for_timeout(2000)
+@pytest.mark.parametrize('email, password', users_invalid_data)
+def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
+    login_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
+    login_page.fill_login_form(email=email, password=password)
+    login_page.click_login_button()
+    login_page.check_visible_wrong_email_or_password_alert()
